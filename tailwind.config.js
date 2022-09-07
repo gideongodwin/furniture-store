a7981b872944cd500672853b9d3165d29539d232
Gideon Godwin <gideongodwin56@gmail.com>
2022-09-07 08:47:46 -0700

    made changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens:{
+      'xs': {'max': '480px'},
+      'sm': '480px',
+      'md': '640px',
+      'lg': '992px',
+      'xl': '1200px'
+    },
     extend: {
       fontFamily: {
         "aura": 'Aurallia',
